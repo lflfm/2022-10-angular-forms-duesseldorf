a20959b390d295dbe38bb39ecb184b239415917e
Ferdinand Malcher <ferdinand@malcher.media>
2022-10-18 09:34:44 +0200

    Formularmodell und Bindings

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators, FormRecord } from '@angular/forms';
 import { AsyncValidatorService } from '../async-validator.service';
 
 @Component({
@@ -8,6 +8,16 @@ import { AsyncValidatorService } from '../async-validator.service';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
+
+  userForm = new FormGroup({
+    username: new FormControl('', { nonNullable: true }),
+    name: new FormControl('', { nonNullable: true }),
+    password: new FormGroup({
+      pw1: new FormControl('', { nonNullable: true }),
+      pw2: new FormControl('', { nonNullable: true }),
+    }),
+    confirm: new FormControl('', { nonNullable: true })
+  });
 
   constructor(private av: AsyncValidatorService) {}
 }
