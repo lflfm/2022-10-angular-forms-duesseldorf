ec89a48729380e08003896475a5d74e7d56bcf8b
Ferdinand Malcher <ferdinand@malcher.media>
2022-10-18 16:10:58 +0200

    Ausblick: Formly

--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -10,5 +10,40 @@ import { AsyncValidatorService } from '../async-validator.service';
 })
 export class DynamicComponent {
 
+  form = new FormGroup({});
+
+  model = {
+    username: 'ferdinand',
+    password: ''
+  };
+
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'username',
+      type: 'input',
+      props: {
+        label: 'Benutzername',
+        placeholder: 'Hier Name eingeben',
+        required: true
+      },
+      asyncValidators: {
+        validation: [this.av.usernameAvailable()]
+      }
+    },
+    {
+      key: 'password',
+      type: 'input',
+      props: {
+        type: 'password',
+        label: 'passwort',
+        description: 'Passwort muss mindestens 8 Zeichen lang sein.',
+        required: false
+      },
+      validators: {
+        validation: [Validators.minLength(8)]
+      }
+    }
+  ];
+
   constructor(private av: AsyncValidatorService) {}
 }
